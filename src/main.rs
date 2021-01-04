dc2e2a766251360ea8f15b54e6e986d158f2af1b
Corralus-Caninus <ward.joshua92@yahoo.com>
2021-01-04 08:10:49 -0800

    dead code removal

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
 #[macro_use]
 extern crate timeit;
-/// This will be a network where weights are sig^weight instead of sig*weight
-/// activations will be the rot_sig_asym (asymmetric sigmoid using bitshifts)
 
-//TODO: test driven development this with unittests as a practice
-//      (project isnt big enough to justify but need to learn)
-// TODO: this is all deprecated
 fn main() {}
 
 #[cfg(test)]
@@ -20,18 +15,13 @@ mod tests {
         //TODO: graph this to csv and visualize in sheets.
         //      write to buffer than flush to disk to time
         //      binops wrt. virtual machine and stack machine
-        //for i in 0..4294967295{
         for i in 1..255 {
-            //if i%100000==0{
-            //info!("\nsent {} got: {}\n",i,rot_act(i));
             info!(
                 "\n sent {} and got: {} == {} \n",
                 i,
                 format!("{:b}", cond_rot_act(i)),
                 cond_rot_act(i)
             );
-            //info!("\ngot: {}\n",format!("{:b}",rot_act(i)));
-            //}
         }
         info!("testing conditional rotation activation..");
         timeit!({
@@ -45,6 +35,5 @@ mod tests {
                 (1.0_f32 / 1.0_f32 + (-1.0_f32 * (i as f32)).exp());
             }
         })
-        //log!("last got: {}",rot_act(4294967295/2));
     }
 }
