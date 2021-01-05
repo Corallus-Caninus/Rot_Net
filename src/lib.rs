05fe4e2291e455c02539ca7294f3c43fc5235571
Corralus-Caninus <ward.joshua92@yahoo.com>
2021-01-05 04:22:08 -0800

    skeleton unittest and Network module

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,22 @@
-
-
 //NOTE: These are the droids you're looking for.
 //32 bit connections
 pub mod connections {
     // going with nodeless topology, activations must occur functionally
     // TODO: network object holds connection boxes? Can ref count here but..
     pub struct Connection<'a> {
-        weight: u8,
-        direction: bool,
-        in_connection: &'a Connection<'a>,
-        out_connection: &'a Connection<'a>,
+        pub weight: u8,
+        pub direction: bool,
+        // TODO: optionals here for frontier nodes.
+        //       these are called in network methods so pub here.
+        pub in_connection: Option<&'a Connection<'a>>,
+        pub out_connection: Option<&'a Connection<'a>>,
     }
     // TODO: attempt normalized connections. after sum normalize prior to activation
     //       (squash then activate-- activation is for network emergent approximation not squashing)
     //       this would mean the entire network is u8 and dont need 32 buffer extension which doesnt
     //       carry representation anyways without a larger activation domain.
     impl<'a> Connection<'a> {
-        pub fn propagate(&mut self, signal: u8) -> u8 {
+        pub fn propagate(&self, signal: u8) -> u8 {
             //implement with rotations and/or masking.
             //This is the primary network bottleneck not activation (secondary).
             if self.direction {
@@ -30,14 +30,14 @@ pub mod connections {
 pub mod activations {
     //      uses 32 bit prec.
     //      hard code a limit to max connections in architecture search or implementation.
-    //      TODO: consider half prec because vectorization is more important at that point 
+    //      TODO: consider half prec because vectorization is more important at that point
     //            than higher precision.
 
     //      consider normalized connections so all values lie within sigmoid feature curves.
     //      this may take away from speed (somewhat.. maybe) but makes the learning much faster.
     //      see: quantization/normalization in ANNs for more (well published)
 
-    //TODO: solve gradient USING THE APPROXIMATION. 
+    //TODO: solve gradient USING THE APPROXIMATION.
 
     ///calculate the activation gradient from an error metric
     pub fn cond_rot_grad(x: u32) -> u8 {
@@ -63,20 +63,17 @@ pub mod activations {
         }
     }
 
-    // TODO: would this precision be as fast with a LUT given binning?
-    //       this is to be scaled out in the domain to >255 anyways
-    //      for more representation so probably not.
     // TODO: expand domain to some value in u32 > u8. calculate connections required to sum to this.
     //       possibly u32? u16 is a good medium u32 will never be reached, requires tens of thousands
     //       of fully activated connections
 
-    /// a conditional piecewise sigmoid approximating sigmoid with 5 line segments
-    /// and using bitshifts for slopes (it's fast-- real fast) ...(can it be faster.?)
+    /// approximates a sigmoid with 5 line segments for inflection points and
+    /// bitshifts for slopes (it's fast-- real fast) ...(can it be faster.?)
     pub fn cond_rot_act(x: u32) -> u8 {
         const SEGMENTS: [u32; 5] = [30, 75, 128, 170, 255];
         //SEGMENTATION DESCRIPTION:
         // 1. tail of sigmoid is somewhat arbitrarily set @ 30 which is 10% of domain.
-        // 2. head of sigmoid is all values >255, NOTE: sigmoid spans parameter precision 
+        // 2. head of sigmoid is all values >255, NOTE: sigmoid spans parameter precision
         //    not buffer precision, this may change.
         // The lines and intercepts are solved for two slopes: 1/2 and 2 given 1. and 2.
         // This approximation is the maximum precision and minimum error optimizing for speed
@@ -103,22 +100,21 @@ pub mod activations {
     }
 }
 
-
 pub mod network {
+    use crate::connections::*;
     pub struct Network<'a> {
         // TODO: consider something more optimal than vec such as numpy array
-        connections: Vec<crate::connections::Connection<'a>>,
+        connections: Vec<Connection<'a>>,
         //inputs and outputs hold Refs therefor need RefCell
-        inputs: Vec<crate::connections::Connection<'a>>,
-        outputs: Vec<crate::connections::Connection<'a>>,
+        inputs: Vec<Connection<'a>>,
+        outputs: Vec<Connection<'a>>,
     }
     impl<'a> Network<'a> {
         //forward propagate through the network
         pub fn cycle(input: Vec<u8>, _output: Vec<u8>) {
             //TODO: take a u8 array and return a u8 array
             let _buffer = vec![input.len()];
-            for _i in input.iter(){
-            }
+            for _i in input.iter() {}
         }
         pub fn add_connection(&mut self, _addition: crate::connections::Connection) {
             //TODO: add a connection to self.
@@ -126,6 +122,11 @@ pub mod network {
             //      k.stanley suggests in emails topology is represented as connections
             //      and this is more space efficient.
             //  consequently this can create hanging nodes and parallel connections.
+        }
+        pub fn forward_propagate(&mut self, inputs: Vec<u8>) -> Vec<u8> {
+            let output: Vec<u8> = vec![];
+            //TODO: propagate and return
+            output
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,34 +6,50 @@ fn main() {}
 #[cfg(test)]
 mod tests {
     use net::activations::*;
+    use net::connections::*;
     use log::info;
 
     #[test]
     fn test_activation() {
-        info!("This is a test of sigmoid activation of 8 bit integer using exclusive bitshift operations.");
+        info!("Testing of sigmoid activation of 8 bit integer using only bitshift operations.");
         info!("target runtime is 3 clock cycles for 3 barrel shift operations.\n");
         //TODO: graph this to csv and visualize in sheets.
         //      write to buffer than flush to disk to time
         //      binops wrt. virtual machine and stack machine
         for i in 1..255 {
             info!(
-                "\n sent {} and got: {} == {} \n",
+                "\n sent {} and got: {} == {}",
                 i,
                 format!("{:b}", cond_rot_act(i)),
                 cond_rot_act(i)
             );
         }
-        info!("testing conditional rotation activation..");
+        info!("Testing conditional rotation activation..");
         timeit!({
             for i in 1..255 {
                 cond_rot_act(i);
             }
         });
-        info!("testing unoptimized full 32x precision sigmoid activation..");
+        info!("Testing unoptimized full 32x precision sigmoid activation..");
         timeit!({
             for i in 1..255 {
                 (1.0_f32 / 1.0_f32 + (-1.0_f32 * (i as f32)).exp());
             }
         })
+    }
+    #[test]
+    fn test_connection_propagation(){
+        info!("Testing forward propagation of a connection with 8 bit shift operations");
+        let connection = Connection{weight: 4 as u8,direction: true,
+            in_connection: None,out_connection: None};
+        for i in 1..255{
+            let solution = connection.propagate(i as u8);
+            // res = connection.propagate()
+            info!("\n sent {} and got {}", i, solution);
+        }
+    }
+    #[test]
+    fn test_network_forward_propagation{
+        info!("Testing forward propagation of a network")
     }
 }
